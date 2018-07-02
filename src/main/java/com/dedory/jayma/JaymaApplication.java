46328f6be464867f70a75b38bf72ca6a806aa842
ayn <layoussou@gmail.com>
2018-07-02 16:25:03 +0000

    first commit

---
+++ src/main/java/com/dedory/jayma/JaymaApplication.java
@@ -0,0 +1,12 @@
+package com.dedory.jayma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaymaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaymaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/dedory/jayma/JaymaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dedory.jayma;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JaymaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
